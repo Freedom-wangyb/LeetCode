0a996e51f1bc67cb15551b3830f1b2d62cbedef6
wangyibo <wangyibo@qipeipu.com>
2020-01-07 17:45:03 +0800

    add 双指针解法

--- question0042/Soultion1.java
+++ question0042/Soultion1.java
@@ -2,8 +2,36 @@ package question0042;
 
 /**
  * 接雨水
+ *
  * @author wangyb
  * @since 2020/1/7
  */
 public class Soultion1 {
+
+    public int trap(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int result = 0;
+        int maxLeft = 0;
+        int maxRight = 0;
+        while (left <= right) {
+            if (height[left] <= height[right]) {
+                if (height[left] >= maxLeft) {
+                    maxLeft = height[left];
+                } else {
+                    result += maxLeft - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] >= maxRight) {
+                    maxRight = height[right];
+                } else {
+                    result += maxRight - height[right];
+                }
+                right--;
+            }
+        }
+
+        return result;
+    }
 }
