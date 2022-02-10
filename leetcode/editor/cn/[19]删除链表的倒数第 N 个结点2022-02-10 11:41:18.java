aa5b320bb5a5280a0a65add37b4defc1a0617d24
wangyibo <wangyibo@shein.com>
2022-02-10 14:11:57 +0800

    leetcode0019

---
+++ leetcode/editor/cn/[19]删除链表的倒数第 N 个结点2022-02-10 11:41:18.java
@@ -0,0 +1,79 @@
+//给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：head = [1,2,3,4,5], n = 2
+//输出：[1,2,3,5]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：head = [1], n = 1
+//输出：[]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：head = [1,2], n = 1
+//输出：[1]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中结点的数目为 sz 
+// 1 <= sz <= 30 
+// 0 <= Node.val <= 100 
+// 1 <= n <= sz 
+// 
+//
+// 
+//
+// 进阶：你能尝试使用一趟扫描实现吗？ 
+// Related Topics 链表 双指针 👍 1780 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        // 删除倒数第n个，要找到倒数第n+1个
+        ListNode tmpNode = findFromEnd(head, n + 1);
+        tmpNode.next = tmpNode.next.next;
+
+        return head;
+    }
+
+    // 找到倒数第n个节点
+    private ListNode findFromEnd(ListNode head, int n) {
+        ListNode p1 = head;
+        ListNode p2 = head;
+
+        for (int i = 0; i < n; i++) {
+            p1 = p1.next;
+        }
+        while (p1.next != null) {
+            // p1,p2一起往前走
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        return p2;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
